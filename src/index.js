21b0c7b73cf2049e8e8f9595acb0e53a3cb3d33e
Bodor István <istvanbodor19@gmail.com>
2022-09-28 14:02:40 +0200

    final commit

--- src/index.js
+++ src/index.js
@@ -2,3 +2,42 @@ import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap/dist/js/bootstrap.bundle.js'
 import './style.css';
 
+
+
+
+function urlvaltas()
+{
+    document.getElementById('kepecske').src = document.getElementById('kepUrl').value;
+}
+
+function szelessegvaltas()
+{
+    document.getElementById('kepecske').width = document.getElementById('kepszelesseg').value;
+}
+function keretvastagsag()
+{
+
+    document.getElementById('kepecske').border = document.getElementById('kepkeretvastagsag').value;
+
+}
+
+
+function keretszin()
+{
+
+    document.getElementById('kepecske').style.borderColor = document.getElementById('kepkeretszin').value;
+
+
+}
+function tonusvaltas()
+{
+    var element = document.body;
+  element.classList.toggle("dark-mode");
+}
+
+document.getElementById('kepkeretszin').addEventListener('input', keretszin)
+document.getElementById('kepkeretvastagsag').addEventListener('input', keretvastagsag)
+document.getElementById('kepUrl').addEventListener('change', urlvaltas);
+document.getElementById('kepszelesseg').addEventListener('input',szelessegvaltas);
+document.getElementById('tonusvalto').addEventListener('click',tonusvaltas);
+
